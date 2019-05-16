bc28a53254bf3623cdb2edb044cf0b1c41e53f53
Miroslav <miro.vd1@abv.bg>
2019-05-16 22:50:51 +0300

    added new module - api

---
+++ api/src/main/java/com/doxito/afkheroes/AfkHeroesApplication.java
@@ -0,0 +1,11 @@
+package com.doxito.afkheroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfkHeroesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AfkHeroesApplication.class, args);
+    }
+}
